88044a4812c6f27ed41bfb7028ef80f1cb5cefb4
00099216 <32801207+00099216@users.noreply.github.com>
2018-06-03 02:10:16 -0600

    Se agregael layout cardview que mostrará noticias. Se cambian los atributos de la clase modelo, porque los anteriores no estaban correctos

---
+++ app/src/main/java/com/andres00099216/parcial2/modelo/Item_new.java
@@ -0,0 +1,77 @@
+package com.andres00099216.parcial2.modelo;
+
+import java.util.Date;
+
+/**
+ * Created by Andres on 3/6/2018.
+ */
+
+public class Item_new {
+    private int id;
+    private String title;
+    private String description;
+    private String coverImage;
+    private Date create_date;
+    private String body;
+    private String game;
+
+
+    public Item_new() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+}
