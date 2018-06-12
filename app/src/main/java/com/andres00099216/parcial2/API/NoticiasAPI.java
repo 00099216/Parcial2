1c278710eee7484f4a3edea07259b19218f58c46
00099216 <32801207+00099216@users.noreply.github.com>
2018-06-12 03:09:37 -0600

    Se termina de momento la actividad NoticiasAPI y se termina la parte del loggin

--- app/src/main/java/com/andres00099216/parcial2/API/NoticiasAPI.java
+++ app/src/main/java/com/andres00099216/parcial2/API/NoticiasAPI.java
@@ -1,8 +1,20 @@
 package com.andres00099216.parcial2.API;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Created by Andres on 12/6/2018.
  */
 
-public class NoticiasAPI {
+public interface NoticiasAPI {
+    String ENDPOINT = "http://gamenewsuca.herokuapp.com";
+
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<String> token(@Field("user") String username, @Field("password") String password);
+
 }
--- app/src/main/java/com/andres00099216/parcial2/Activities/LoginActivity.java
+++ app/src/main/java/com/andres00099216/parcial2/Activities/LoginActivity.java
@@ -1,39 +1,85 @@
 package com.andres00099216.parcial2.Activities;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.andres00099216.parcial2.API.Deserializadores.TokenDes;
+import com.andres00099216.parcial2.API.NoticiasAPI;
 import com.andres00099216.parcial2.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.net.SocketTimeoutException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     private Button boton;
     private EditText usuario, contraseña;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
         usuario = findViewById(R.id.username);
-        contraseña =findViewById(R.id.contraseña);
+        contraseña = findViewById(R.id.contraseña);
         boton = findViewById(R.id.boton_entrar);
 
         boton.setOnClickListener(v -> Click());
     }
 
-    public void Click(){
-        if (usuario.getText().toString().equals("") || contraseña.getText().toString().equals("")){
+    public void Click() {
+        if (usuario.getText().toString().equals("") || contraseña.getText().toString().equals("")) {
             Toast.makeText(this, "Fields can't be empty", Toast.LENGTH_SHORT).show();
 
-        }else {
+        } else {
+            Gson gson = new GsonBuilder().registerTypeAdapter(String.class, new TokenDes()).create();
+            Retrofit.Builder builder = new Retrofit.Builder().baseUrl(NoticiasAPI.ENDPOINT).addConverterFactory(GsonConverterFactory.create(gson));
+            Retrofit retrofit = builder.build();
+            NoticiasAPI apiGameNews = retrofit.create(NoticiasAPI.class);
+
+            Call<String> stringCall = apiGameNews.token(usuario.getText().toString(), usuario.getText().toString());
+
+            stringCall.enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    if (response.isSuccessful() && !response.body().equals("")) {
+
+                        startMainActivity();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error: check later", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    if (t instanceof SocketTimeoutException) {
+
+                        Toast.makeText(LoginActivity.this, "Timed Out", Toast.LENGTH_SHORT).show();
+                    } else if (t instanceof Exception) {
+                        Toast.makeText(LoginActivity.this, "There was an error. \n Please try again later", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
         }
 
+    }
+
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
