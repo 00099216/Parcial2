9d2390c58626e752b0115a3ea276926b1dfe68ea
00099216 <32801207+00099216@users.noreply.github.com>
2018-06-12 02:05:01 -0600

    Se crea una nueva clase dentro de la carpeta API, llamada NoticiasAPI. Se avanza en la actividad para loguearse

---
+++ app/src/main/java/com/andres00099216/parcial2/API/NoticiasAPI.java
@@ -0,0 +1,8 @@
+package com.andres00099216.parcial2.API;
+
+/**
+ * Created by Andres on 12/6/2018.
+ */
+
+public class NoticiasAPI {
+}
--- app/src/main/java/com/andres00099216/parcial2/Activities/LoginActivity.java
+++ app/src/main/java/com/andres00099216/parcial2/Activities/LoginActivity.java
@@ -1,31 +1,39 @@
 package com.andres00099216.parcial2.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.andres00099216.parcial2.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button boton;
+    private EditText usuario, contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        Button Boton;
-        Boton = findViewById(R.id.boton_entrar);
+        usuario = findViewById(R.id.username);
+        contraseña =findViewById(R.id.contraseña);
+        boton = findViewById(R.id.boton_entrar);
 
-        Boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        boton.setOnClickListener(v -> Click());
+    }
 
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                v.getContext().startActivity(intent);
-            }
-        });
+    public void Click(){
+        if (usuario.getText().toString().equals("") || contraseña.getText().toString().equals("")){
+            Toast.makeText(this, "Fields can't be empty", Toast.LENGTH_SHORT).show();
+
+        }else {
+
+        }
 
     }
 }
