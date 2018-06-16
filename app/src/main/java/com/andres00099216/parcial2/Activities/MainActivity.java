93ec06be99bc6a480d44efb4769721d04d5d649c
00099216 <32801207+00099216@users.noreply.github.com>
2018-06-15 20:41:20 -0600

    Se agrega el menu, aunque no tiene nada de funcionalidad

--- app/src/main/java/com/andres00099216/parcial2/Activities/MainActivity.java
+++ app/src/main/java/com/andres00099216/parcial2/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,8 +18,7 @@ import com.andres00099216.parcial2.fragmentos.NoticiaFragmento;
 public class MainActivity extends AppCompatActivity {
     db gameNewsDatabase;
     DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,33 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.main_activity);
 
-        drawerLayout = findViewById(R.id.drawerLayout);
-        navigationView = findViewById(R.id.navigationView);
-        toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.icono_menu);
 
-        initMain();
+        drawerLayout = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.getMenu().getItem(0).setChecked(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
 
-    }
-    private void initMain() {
+                return true;
+            }
+        });
+        //Se crea el fragmento de noticias
         Fragment fragment = new NoticiaFragmento();
         getSupportFragmentManager().beginTransaction().replace(R.id.contentLayout, fragment).commit();
         getSupportActionBar().setTitle("Noticias");
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icono_menu);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 
 }
